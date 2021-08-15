c6f2695d30c5202af1d9280ee092061b3fc15876
menuka <menuka.premabandu@yahoo.com>
2021-08-16 09:00:38 +1000

    typo

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,7 +8,7 @@ function HeroSection() {
     return (
       <div className='hero-container'>
         <video src='/videos/main.mp4' autoPlay loop muted />
-        <h1>Your favoirte pics online</h1>
+        <h1>Your favourite pics online</h1>
         <p>Upload Your Images Now</p>
         <UploadForm/>
         {/* <div className='hero-btns'>
